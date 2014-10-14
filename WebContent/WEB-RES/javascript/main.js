bd5c84f583e2cc0a2f2573771f9e9aad8e98b5ff
GROX13 <giorgirokhadze@gmail.com>
2014-10-14 19:43:14 +0400

    Added homepage.

---
+++ WebContent/WEB-RES/javascript/main.js
@@ -0,0 +1,47 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Parallax
+$(window).stellar();
+
+// Carousel
+$('.carousel').carousel({
+    pause: true,
+    interval: false
+});
+
+$("video").prop('muted', true);
+$('.contact-video').coverVid(1920, 1080);
+
+
+  // Auto hiding toggle menu
+  $(function() {
+    $('.nav a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+  });
